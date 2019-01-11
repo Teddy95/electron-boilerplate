dca2ddd9f9e7c6acc21b483465d682bf854c7ab0
Andre Sieverding <webmaster@andre-sieverding.de>
2019-01-11 08:50:37 +0100

    Update main.js

--- app/main.js
+++ app/main.js
@@ -14,7 +14,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: 'build/assets/icons/icon.png'})
 
   // and load the index.html of the app.
   mainWindow.loadFile('build/index.html')
